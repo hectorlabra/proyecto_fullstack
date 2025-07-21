f0443c6cfaa58c0ad8749015cce6f51985d56aca
Hector Labra <hlabrablistavap@gmail.com>
2025-07-21 17:31:10 -0400

    feat(front): aplicar estilos CSS a componentes Navbar y Home

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "../styles/Home.css";
 
 function Home() {
   return (
     <div className="home-container">
       <Navbar />
-      <h1>HOME</h1>
+      <main className="main-content">
+        <h1 className="home-title">HOME</h1>
+      </main>
     </div>
   );
 }
