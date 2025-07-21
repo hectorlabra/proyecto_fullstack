c93d82b64b2919cf9533d7eef9086adab20b5699
Hector Labra <hlabrablistavap@gmail.com>
2025-07-21 17:20:09 -0400

    feat(front): agregar componente Home e integración con App

---
+++ front/src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>HOME</h1>
+      {/* Aquí se renderizará el Navbar */}
+    </div>
+  );
+}
+
+export default Home;
