29464c283e15bdc22550fbaeea0f81f252bee23f
Hector Labra <hlabrablistavap@gmail.com>
2025-10-03 14:49:22 -0300

    feat(ui): Agregar index de exportaciones para componentes UI
    - Centralizar exports de todos los componentes
    - Facilitar imports: import { Button, Modal } from './ui'
    - Incluir todos los componentes y sub-componentes
    - AC: Build OK, exports funcionando

---
+++ front/src/components/ui/index.js
@@ -0,0 +1,23 @@
+// UI Components - Healthcare Design System
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { 
+  Card, 
+  CardHeader, 
+  CardTitle, 
+  CardSubtitle, 
+  CardContent, 
+  CardFooter, 
+  CardActions 
+} from "./Card";
+export { Badge, StatusBadge } from "./Badge";
+export { 
+  Skeleton, 
+  AppointmentCardSkeleton, 
+  AppointmentListSkeleton 
+} from "./Skeleton";
+export { Spinner, SpinnerContainer, InlineSpinner } from "./Spinner";
+export { ToastProvider } from "./Toast";
+export { useToast } from "./toast-context";
+export { Breadcrumbs } from "./Breadcrumbs";
+export { Modal, ConfirmModal } from "./Modal";
